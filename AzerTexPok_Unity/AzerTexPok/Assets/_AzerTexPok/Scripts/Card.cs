759363beb380216ac49e772df8445d658e96dcd6
bochelord <bochelord@gmail.com>
2019-11-07 18:01:42 +0100

    [Scene + code] Added Card Class and ScriptableObjects script associated

---
+++ AzerTexPok_Unity/AzerTexPok/Assets/_AzerTexPok/Scripts/Card.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public enum Value { two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace };
+public enum Color { hearts, diamonds, spades, clubs }
+
+[Serializable]
+public class Card : IComparable
+{
+
+
+
+    public Value cardValue;
+    public Color cardColor;
+
+    /// <summary>
+    /// Icomparable interface
+    /// </summary>
+    /// <param name="obj">object to compare</param>
+    /// <returns>-1 < obj, 0 = obj, 1 > obj </returns>
+    public int CompareTo(object obj)
+    {
+        
+        if (obj is Card)
+        {
+            return this.cardValue.CompareTo((obj as Card).cardValue);
+        }
+        else
+        {
+            throw new ArgumentException("Object to compare is not a Card");
+        }
+
+
+
+    }
+
+    
+}
---
+++ AzerTexPok_Unity/AzerTexPok/Assets/_AzerTexPok/Scripts/ScriptableObjectSource/Card_SO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class Card_SO : ScriptableObject
+{
+
+    public Image cardImage;
+
+    public Card card;
+
+
+
+}
